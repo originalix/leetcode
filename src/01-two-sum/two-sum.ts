dc6c442a0cd0d7d72d55d9ea0c10d6fb7b1f1c54
jiaojiansen <jiaojiansen@deyuanqiche.com>
2021-07-02 13:57:07 +0800

    test(测试): 测试提交

--- src/01-two-sum/two-sum.ts
+++ src/01-two-sum/two-sum.ts
@@ -2,6 +2,7 @@ export default function twoSum(nums: number[], target: number): number[] {
   const map = new Map()
   for (let i = 0; i < nums.length; i++) {
     if (map.has(target - nums[i])) {
+      console.log(i)
       return [map.get(target - nums[i]), i]
     }
     map.set(nums[i], i)
